8004afe08fcc0c7f7ae41804b35b7953a0afa03b
nks102000 <nitishks1110@gmail.com>
2021-03-03 20:03:24 +0530

    Navigation bar added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,47 @@
-import React from 'react'
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+    Button,
+    Form,
+    FormControl,
+    Nav,
+    Navbar,
+    NavDropdown,
+} from "react-bootstrap";
 
-const Header = () => {
+import { link, NavLink, withRouter } from "react-router-dom";
+
+import {
+    HomeRounded,
+    SchoolRounded,
+    WorkRounded,
+    Facebook,
+    Twitter,
+    LinkedIn,
+    Github,
+    Telegram,
+} from "@material-ui/icons";
+
+const Header = (props) => {
+    const pathName = props?.location?.pathname;
+
     return (
-        <div>
-            This is header
-        </div>
+        <Navbar expand="lg" sticky='top' className="header">
+            <Nav.Link as={NavLink} to="/">
+                <Navbar.Brand className="header_home"><HomeRounded /></Navbar.Brand>
+            </Nav.Link>
+
+            <Navbar.Toggle />
+
+            <Navbar.Collapse>
+                <Nav>
+                    <Nav.Link as={NavLink} to="/" classname={pathName == '/' ? 'header_link_active' 
+                    : "header_link"}>Resume</Nav.Link>
+                    <Nav.Link as={NavLink} to="/portfolio" classname={pathName == '/portfolio' ? 
+                    'header_link_active' : "header_link"}>Portfolio</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
