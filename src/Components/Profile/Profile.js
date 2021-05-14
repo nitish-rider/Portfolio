e393a673c894fc34d3d6a26a816ff1958ae387d2
nks102000 <nitishks1110@gmail.com>
2021-05-14 22:27:39 +0530

    Resume

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -10,73 +10,102 @@ import profile from "../../assets/images/profile1.jpg";
 
 import resumeDetails from "../../utils/resumeDetails";
 import PersonIcon from "@material-ui/icons/Person";
-import GetAppIcon from '@material-ui/icons/GetApp';
+import GetAppIcon from "@material-ui/icons/GetApp";
 
-import CustomButton from '../Button/Button'
+import CustomButton from "../Button/Button";
 
-
-const TimeLineItem = ({ title, text, link }) => (
-    <TimelineItem>
-        <TimeLineSeparator />
-        <TimelineContent>
-            {link ? (
-                <Typography>
-                    <span> {title}: </span>{" "}
-                    <a href={link} target="_blank">
-                        {text}
-                    </a>
-                </Typography>
-            ) : (
-                <Typography className="timelineItem_text">
-                    <span className="text_info"> {title}: </span> {text}
-                </Typography>
-            )}
-        </TimelineContent>
-    </TimelineItem>
+const TimeLineItem = ({ title, text, link }) => ( <
+    TimelineItem >
+    <
+    TimeLineSeparator / >
+    <
+    TimelineContent > { " " } {
+        link ? ( <
+            Typography >
+            <
+            span > { title }: < /span>{" "} <
+            a href = { link }
+            target = "_blank" > { " " } { text } { " " } <
+            /a>{" "} <
+            /Typography>
+        ) : ( <
+            Typography className = "timelineItem_text" >
+            <
+            span className = "text_info" > { title }: < /span> {text}{" "} <
+            /Typography>
+        )
+    } { " " } <
+    /TimelineContent>{" "} <
+    /TimelineItem>
 );
 
 const Profile = () => {
-    return (
-        <div  className="profile container_shadow margin">
-            <div className="profile_name">
-                <Typography className="name">{resumeDetails.name}</Typography>
-                <Typography className="title">{resumeDetails.title}</Typography>
-            </div>
-
-            <figure className="profile_image">
-                <img src={profile} alt="" />
-            </figure>
-            <div className="profile_information">
-                <TimeLine icon={<PersonIcon />}>
-                    <TimeLineItem title="Name" text={resumeDetails.name} />
-                    <TimeLineItem title="Email" text={resumeDetails.email} />
-                    <TimeLineItem title="Phone" text={resumeDetails.phone} />
-                    <TimeLineItem title="Job" text={resumeDetails.jobs} />
-                    <TimeLineItem title="Address" text={resumeDetails.address} />
-                    {/* {Object.keys(resumeDetails.social).map((key) => (
-                    <TimeLineItem
-                        title={key}
-                        text={resumeDetails.social[key].text}
-                        link={resumeDetails.social[key].link}
-                    />
-                ))} */}
-                </TimeLine>
-                <br />
-                
-                <div className = "button_container">
-                <a href="https://drive.google.com/file/d/1_3kEgBHFGaeNyJHMBoVVa4uQd3wnoj7K/view?usp=sharing" target="_blank"><CustomButton text = {'Download Resume'} icon = {<GetAppIcon />}  link = {""}/></a>
-                </div> 
-
-                {/* <Button
-                    icon={ CloudDownloadOutlinedIcon }
-                    text="Get Resume"
-                ></Button> */}
-
-                {/* <button className="getresumebutton">Download Resume 
-                <i class="fa fa-download download_icon" aria-hidden="true"></i>
-                </button> */}
-            </div>
-        </div>
+    return ( <
+        div className = "profile container_shadow margin" >
+        <
+        div className = "profile_name" >
+        <
+        Typography className = "name" > { resumeDetails.name } < /Typography>{" "} <
+        Typography className = "title" > { resumeDetails.title } < /Typography>{" "} <
+        /div> <
+        figure className = "profile_image" >
+        <
+        img src = { profile }
+        alt = "" / >
+        <
+        /figure>{" "} <
+        div className = "profile_information" >
+        <
+        TimeLine icon = { < PersonIcon / > } >
+        <
+        TimeLineItem title = "Name"
+        text = { resumeDetails.name }
+        />{" "} <
+        TimeLineItem title = "Email"
+        text = { resumeDetails.email }
+        />{" "} <
+        TimeLineItem title = "Phone"
+        text = { resumeDetails.phone }
+        />{" "} <
+        TimeLineItem title = "Job"
+        text = { resumeDetails.jobs }
+        />{" "} <
+        TimeLineItem title = "Address"
+        text = { resumeDetails.address }
+        />{" "} {
+            /* {Object.keys(resumeDetails.social).map((key) => (
+                                            <TimeLineItem
+                                                title={key}
+                                                text={resumeDetails.social[key].text}
+                                                link={resumeDetails.social[key].link}
+                                            />
+                                        ))} */
+        } { " " } <
+        /TimeLine>{" "} <
+        br / >
+        <
+        div className = "button_container" >
+        <
+        a href = "https://drive.google.com/file/d/1N6U5vPaWXnWgKQnJUyeadmr9lItLMfie/view?usp=sharing"
+        target = "_blank" >
+        { " " } <
+        CustomButton text = { "Download Resume" }
+        icon = { < GetAppIcon / > }
+        link = { "" }
+        /> <
+        /a> <
+        /div> {
+            /* <Button
+                                        icon={ CloudDownloadOutlinedIcon }
+                                        text="Get Resume"
+                                    ></Button> */
+        } {
+            /* <button className="getresumebutton">Download Resume 
+                                    <i class="fa fa-download download_icon" aria-hidden="true"></i>
+                                    </button> */
+        } { " " } <
+        /div>{" "} <
+        /div>
     );
 };
 
